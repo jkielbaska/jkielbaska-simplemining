19ccdcc8f274b5aa4ccdebc3584b8e4042a4393a
Jakub <kielbaskakuba@gmail.com>
2024-01-20 19:41:36 +0100

    Add useFaq custom hook to fetch data from api, write function groupFaqData to group api response

---
+++ src/hooks/useFaq.ts
@@ -0,0 +1,60 @@
+import { FaqData, GrouppedFaqData } from "../types/faqData";
+import { getFaqData } from "../services/api";
+import { useEffect, useState } from "react";
+
+export const useFaq = () => {
+  const [faqData, setFaqData] = useState<FaqData | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null | unknown>(null);
+
+  const fetchFaqData = async () => {
+    setIsLoading(true);
+    try {
+      const data = await getFaqData();
+      setFaqData(data);
+    } catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFaqData();
+  }, []);
+
+  const groupFaqData = (faqData: FaqData | null) => {
+    const grouppedData: { left: GrouppedFaqData; right: GrouppedFaqData } = {
+      left: {},
+      right: {},
+    };
+
+    faqData?.questions.forEach((question) => {
+      const group =
+        faqData.groups.left.find((group) => group.id === question.groupId) ||
+        faqData.groups.right.find((group) => group.id === question.groupId);
+
+      if (group) {
+        if (!grouppedData[group.columnType][question.groupId]) {
+          grouppedData[group.columnType][question.groupId] = {
+            name: group.name,
+            questions: [],
+          };
+        }
+
+        grouppedData[group.columnType][question.groupId].questions.push(
+          question
+        );
+      }
+    });
+    return grouppedData;
+  };
+  const grouppedFaqData = groupFaqData(faqData);
+  console.log(grouppedFaqData);
+
+  return {
+    faqData,
+    grouppedFaqData,
+    isLoading,
+    error,
+  };
+};
