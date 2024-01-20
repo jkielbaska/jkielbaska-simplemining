857b1b55dd2de892986ce8d78cbcdbb945975a6a
Jakub <kielbaskakuba@gmail.com>
2024-01-20 19:44:00 +0100

    Check if code from useFaq custom hook work. Display whole grouppedFaqData

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,45 @@
+import { useFaq } from "./hooks/useFaq";
+import { GrouppedFaqData } from "./types/faqData";
+import dompurify from "dompurify";
+
 function App() {
+  const { grouppedFaqData, isLoading, error } = useFaq();
+  const sanitizer = dompurify.sanitize;
+  const renderFaqData = (grouppedFaqData: GrouppedFaqData) => {
+    return Object.keys(grouppedFaqData).map((key) => {
+      const group = grouppedFaqData[key];
+      return (
+        <div key={key}>
+          <h2>{group.name}</h2>
+          <ul>
+            {group.questions.map((question) => (
+              <li key={question.id}>
+                <h3>{question.title}</h3>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: sanitizer(question.content),
+                  }}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    });
+  };
+
   return (
     <>
-      <p className="test">Hello world</p>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error</p>
+      ) : (
+        <>
+          <div>{renderFaqData(grouppedFaqData.left)}</div>
+          <div>{renderFaqData(grouppedFaqData.right)}</div>
+        </>
+      )}
     </>
   );
 }
