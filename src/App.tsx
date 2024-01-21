461365d3091a55abdf974ed6ca51054423e5ceba
Jakub <kielbaskakuba@gmail.com>
2024-01-21 15:54:58 +0100

    Add input to handle search from useFilteredFaq, add Column components and pass data from faqData and filteredFaqQuestions, comment previous code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import { useFaq } from "./hooks/useFaq";
-import { GrouppedFaqData } from "./types/faqData";
-import dompurify from "dompurify";
-
+import { useFilteredFaq } from "./hooks/useFilteredFaq";
+import { Column } from "./components/Column";
 function App() {
-  const { grouppedFaqData, isLoading, error } = useFaq();
-  const sanitizer = dompurify.sanitize;
-  const renderFaqData = (grouppedFaqData: GrouppedFaqData) => {
-    return Object.keys(grouppedFaqData).map((key) => {
-      const group = grouppedFaqData[key];
-      return (
-        <div key={key}>
-          <h2>{group.name}</h2>
-          <ul>
-            {group.questions.map((question) => (
-              <li key={question.id}>
-                <h3>{question.title}</h3>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: sanitizer(question.content),
-                  }}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-      );
-    });
-  };
+  const { faqData, isLoading, error } = useFaq();
+  const { query, setQuery, filteredFaqQuestions } = useFilteredFaq(
+    faqData || { groups: { left: [], right: [] }, questions: [] }
+  );
 
   return (
     <>
@@ -36,12 +15,59 @@ function App() {
         <p>Error</p>
       ) : (
         <>
-          <div>{renderFaqData(grouppedFaqData.left)}</div>
-          <div>{renderFaqData(grouppedFaqData.right)}</div>
+          <input
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            type="search"
+          />
+          {filteredFaqQuestions?.length ? (
+            <>
+              <Column
+                groups={faqData?.groups.left}
+                questions={filteredFaqQuestions}
+              />
+              <Column
+                groups={faqData?.groups.right}
+                questions={filteredFaqQuestions}
+              />
+            </>
+          ) : (
+            <div>
+              <h2>No results found</h2>
+              <p>
+                make sure if spellng is correct or try with different keywords
+              </p>
+            </div>
+          )}
         </>
       )}
     </>
   );
 }
-
+{
+  /* <Group grouppedFaqData={grouppedFaqData.left} />
+    <Group grouppedFaqData={grouppedFaqData.right} /> */
+}
 export default App;
+// return keys.some((key) =>
+// item[key].toLowerCase().includes(query.toLowerCase())
+// );
+
+//every word in input should be included in title or content
+//
+// const filteredFaqQuestions = useMemo(() => {
+//   if (!query) {
+//     return faqData?.questions;
+//   }
+
+//   const queryWords = query.toLowerCase().split(" ");
+
+//   return faqData?.questions.filter((item) => {
+//     const titleWords = item.title.toLowerCase();
+//     const contentWords = item.content.toLowerCase();
+
+//     return queryWords.every(
+//       (word) => titleWords.includes(word) || contentWords.includes(word)
+//     );
+//   });
+// }, [query, faqData]);
