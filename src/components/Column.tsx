b82d400be2be15f99ac6287fd524a9cbb9ec6afb
Jakub <kielbaskakuba@gmail.com>
2024-01-21 15:45:34 +0100

    Add Column component to filter questions, group them and display in a correct way

---
+++ src/components/Column.tsx
@@ -0,0 +1,36 @@
+import { TQuestion, TGroup } from "../types/faqData";
+import { GroupQuestions } from "./GroupQuestions";
+
+export const Column = ({
+  groups,
+  questions,
+}: {
+  groups: TGroup[] | undefined;
+  questions: TQuestion[] | undefined;
+}) => {
+  const filterQuestions = (group: TGroup, questions: TQuestion[] | undefined) =>
+    questions ? questions.filter((el) => el.groupId === group.id) : [];
+
+  return (
+    <div>
+      {groups?.map((group) => (
+        <GroupQuestions
+          group={group}
+          key={group.id}
+          groupQuestions={filterQuestions(group, questions)}
+        />
+      ))}
+    </div>
+  );
+};
+// groupQuestions={filterQuestions(group, questions).filter((el) => {
+//   return query.toLowerCase() === ""
+//     ? el
+//     : el.title.toLowerCase().includes(query.toLowerCase());
+// })}
+
+// groupQuestions={filterQuestions(group, questions).filter((el) =>
+//   keys.some((key) =>
+//     el[key].toLowerCase().includes(query.toLowerCase())
+//   )
+// )}
