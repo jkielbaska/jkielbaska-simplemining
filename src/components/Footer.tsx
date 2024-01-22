766e00776fbf7473d41e44c75874d5448ae669b1
Jakub <kielbaskakuba@gmail.com>
2024-01-22 11:26:07 +0100

    Add Header and Footer components, add styles to Header and Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        made by{" "}
+        <a
+          target="_blank"
+          href="https://github.com/jkielbaska"
+          className="bold hover"
+        >
+          jkielbaska
+        </a>
+      </div>
+      <div>
+        icons from{" "}
+        <a target="_blank" href="https://icons8.com/" className="bold hover">
+          icons8.com
+        </a>
+      </div>
+    </footer>
+  );
+};
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import faqIcon from "../assets/icons8-faq-100.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <img width={50} height={50} src={faqIcon} alt="faqIcon icon" />
+      <h1>Help center</h1>
+    </header>
+  );
+};
